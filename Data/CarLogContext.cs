f3f1be00e22b7b384867e832fdf84d5a3b5c29dd
= <overt.coffee@gmail.com>
2020-09-12 14:41:33 -0500

    Add db context

---
+++ Data/CarLogContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace car_log.Data
+{
+    public class CarLogContext : DbContext
+    {
+        public CarLogContext (DbContextOptions<CarLogContext> options)
+        : base(options)
+        {
+
+        }
+        public DbSet<car_log.Models.Vehicle> Vehicle { get; set; }
+        public DbSet<car_log.Models.Recorc> Record { get; set; }
+    }
+}
