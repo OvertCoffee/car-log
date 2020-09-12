43af4c282d4e0172c3b9cb7adb5c986a8f20f18e
= <overt.coffee@gmail.com>
2020-09-12 16:53:17 -0500

    Add Migrations, add database to .gitignore

---
+++ Migrations/20200912214543_InitialMigration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace car_log.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Record",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Time = table.Column<DateTime>(nullable: false),
+                    Location = table.Column<string>(nullable: true),
+                    Odometer = table.Column<int>(nullable: false),
+                    Trip = table.Column<double>(nullable: false),
+                    Gallons = table.Column<double>(nullable: false),
+                    Cost = table.Column<double>(nullable: false),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Record", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Type = table.Column<int>(nullable: false),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Record");
+
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
