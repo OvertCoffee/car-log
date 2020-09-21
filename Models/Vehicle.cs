6463234ca1e6e8d07a631eb5f1cc410f5812e9fd
ben <bhenderson@wwlinc.com>
2020-09-21 13:37:36 -0500

    Make vehicle type string

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -5,12 +5,7 @@ namespace car_log.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public VehicleType Type { get; set; }
+        public string Type { get; set; }
         public string Notes { get; set; }
-    }
-    public enum VehicleType
-    {
-        Automobile,
-        Plane
     }
 }
